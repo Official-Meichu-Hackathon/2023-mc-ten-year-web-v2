4379fe466d69cd97577de8ae4876b53f6c56333a
Ritmo <jajh102098@gmail.com>
2023-07-15 04:58:42 +0800

    Fix deploy error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: "build",
+    // distDir: "build",
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "farm66.staticflickr.com",
+                port: "",
+                pathname: "/65535/**"
+            },
+            {
+                protocol: "https",
+                hostname: "live.staticflickr.com",
+                port: "",
+                pathname: "/65535/**"
+            }
+        ]
+    },
+    "fontawesome-svg-core": {
+        "license": "free"
+    }
 }
 
 module.exports = nextConfig
