d1c8c8b025367bb768b767e20c09eb87dffb10a2
YI-CHEN\sunny <sunny930817@gmail.com>
2023-07-30 18:20:59 +0800

    feat: delete tags

--- src/app/components/QApage/Question.js
+++ src/app/components/QApage/Question.js
@@ -18,20 +18,9 @@ export default function Questions({ data }) {
           {data.map((q, index) => (
               <li key={index} className="grid gap-y-8 nue-concave frost-25 p-12 rounded-[4rem] ">
                 <div className="flex flex-col justify-center items-center">
-                  <h2 className="text-primary-gradient text-center">Q: {q.question}</h2>
+                  <h2 className="text-primary-gradient text-center mb-5">Q: {q.question}</h2>
                   <div className="grid gap-y-4">
-                    <ul className="flex list-none">
-                      {q.tags.map((tag, index) => (
-                          <li
-                              key={index}
-                              className={`${index !== q.tags.length - 1 ? "mr-3" : ""} p-1 rounded-[0.375em] capitalize bg-secondary`}
-                          >
-                            {tag}
-                          </li>
-                      ))}
-                    </ul>
                     {q.answers.map((answer, index) => (
-
                         <p key={index}>
                           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                           {answer.length > 80 && selectedQuestion !== q
