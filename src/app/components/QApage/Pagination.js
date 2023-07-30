e6f1c63628069dcd1c4f3298ad8399f3806e070e
YI-CHEN\sunny <sunny930817@gmail.com>
2023-07-30 16:57:56 +0800

    feat: add pagination (default 2 cards/page)

---
+++ src/app/components/QApage/Pagination.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default function Pagination({ totalPages, currentPage, onPageChange }) {
+  return (
+      <div className="flex justify-center mt-5">
+        <button
+            className="mr-3 p-2 cursor-pointer"
+            disabled={currentPage === 1}
+            onClick={() => onPageChange(currentPage - 1)}
+        >
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14 7L9 12L14 17V7Z" fill="black"/>
+          </svg>
+
+        </button>
+        {Array.from({ length: totalPages }, (_, i) => (
+            <span
+                key={i}
+                onClick={() => onPageChange(i + 1)}
+                className={`mr-2 p-2 cursor-pointer ${
+                    currentPage === i + 1 ? 'font-bold' : ''
+                }`}
+            >
+          {i + 1}
+        </span>
+        ))}
+        <button
+            className="p-2 cursor-pointer"
+            disabled={currentPage === totalPages}
+            onClick={() => onPageChange(currentPage + 1)}
+        >
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 17L15 12L10 7V17Z" fill="black"/>
+          </svg>
+
+        </button>
+      </div>
+  );
+}
--- src/app/qna/page.js
+++ src/app/qna/page.js
@@ -1,22 +1,45 @@
 "use client";
-import useSWR from "swr";
-import { Load, LoadFailed } from "../components/gadgets"
-import Questions from "@/app/components/QApage/Question";
+import React, { useState } from 'react';
+import useSWR from 'swr';
+import { Load, LoadFailed } from '../components/gadgets';
+import Questions from '@/app/components/QApage/Question';
+import Pagination from '@/app/components/QApage/Pagination';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
-export default function Qna() {
-    const { data, error } = useSWR("/api/qna", fetcher);
-    if(error) return <LoadFailed />;
-	if(!data) return <Load />;
 
-    return (
-        <div>
-            <header className="flex justify-center mb-5">
-                <h1 className="bracket-md text-center">常見問題<br/>Q & A</h1>
-            </header>
-            <main>
-              <Questions data={data} />
-            </main>
-        </div>
-    );
+export default function Qna() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 2;
+
+  const { data, error } = useSWR('/api/qna', fetcher);
+  if (error) return <LoadFailed />;
+  if (!data) return <Load />;
+
+  const totalItems = data.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentData = data.slice(startIndex, endIndex);
+
+  const changePage = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  return (
+      <div>
+        <header className="flex justify-center mb-5">
+          <h1 className="bracket-md text-center">常見問題<br />Q & A</h1>
+        </header>
+        <main>
+          <Questions data={currentData} />
+          <Pagination
+              totalPages={totalPages}
+              currentPage={currentPage}
+              onPageChange={changePage}
+          />
+        </main>
+      </div>
+  );
 }
