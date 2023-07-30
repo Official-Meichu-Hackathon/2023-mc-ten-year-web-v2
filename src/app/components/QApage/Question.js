13628632acfc7079abf9af9f71f42cb8edf931e8
YI-CHEN\sunny <sunny930817@gmail.com>
2023-07-30 17:25:44 +0800

    feat: add some detail on QA card

--- src/app/components/QApage/Question.js
+++ src/app/components/QApage/Question.js
@@ -16,34 +16,38 @@ export default function Questions({ data }) {
       <div>
         <ul className="grid gap-20 md:grid-cols-2">
           {data.map((q, index) => (
-              <li key={index} className="grid gap-y-8 nue-concave frost-25 p-12 rounded-[4rem]">
-                <h2 className="text-primary-gradient text-center">Q: {q.question}</h2>
-                <div className="grid gap-y-4">
-                  <ul className="flex list-none">
-                    {q.tags.map((tag, index) => (
-                        <li
-                            key={index}
-                            className={`${index !== q.tags.length - 1 ? "mr-3" : ""} p-1 rounded-[0.375em] capitalize bg-secondary`}
-                        >
-                          {tag}
-                        </li>
+              <li key={index} className="grid gap-y-8 nue-concave frost-25 p-12 rounded-[4rem] ">
+                <div className="flex flex-col justify-center items-center">
+                  <h2 className="text-primary-gradient text-center">Q: {q.question}</h2>
+                  <div className="grid gap-y-4">
+                    <ul className="flex list-none">
+                      {q.tags.map((tag, index) => (
+                          <li
+                              key={index}
+                              className={`${index !== q.tags.length - 1 ? "mr-3" : ""} p-1 rounded-[0.375em] capitalize bg-secondary`}
+                          >
+                            {tag}
+                          </li>
+                      ))}
+                    </ul>
+                    {q.answers.map((answer, index) => (
+
+                        <p key={index}>
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          {answer.length > 80 && selectedQuestion !== q
+                              ? answer.slice(0, 80) + '...'
+                              : + answer}
+                        </p>
                     ))}
-                  </ul>
-                  {q.answers.map((answer, index) => (
-                      <p key={index}>
-                        {answer.length > 80 && selectedQuestion !== q
-                            ? answer.slice(0, 80) + '...'
-                            : answer}
-                      </p>
-                  ))}
-                  {q.answers.length > 0 && (
-                      <button
-                          className="text-primary-gradient mt-2"
-                          onClick={() => openPopup(q)}
-                      >
-                        {selectedQuestion === q ? '收起回答/Collapse' : '查看更多/View more...'}
-                      </button>
-                  )}
+                    {q.answers.length > 0 && (
+                        <button
+                            className="text-primary-gradient mt-2 text-[#809FFF]"
+                            onClick={() => openPopup(q)}
+                        >
+                          {selectedQuestion === q ? '收起回答/Collapse' : '查看更多/View more...'}
+                        </button>
+                    )}
+                  </div>
                 </div>
               </li>
           ))}
