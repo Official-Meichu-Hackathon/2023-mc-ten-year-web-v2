890f6a5770516a90ac92082c1f788d39cff7b1c2
YI-CHEN\sunny <sunny930817@gmail.com>
2023-07-31 21:31:05 +0800

    feat: change items per page

--- src/app/qna/page.js
+++ src/app/qna/page.js
@@ -3,13 +3,14 @@ import React, { useState } from 'react';
 import useSWR from 'swr';
 import { Load, LoadFailed } from '../components/gadgets';
 import Questions from '@/app/components/QApage/Question';
-import Pagination from '@/app/components/QApage/Pagination';
+import Pagination from '@/app/components/Pagination';
+import Filter from "@/app/components/QApage/Filter";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Qna() {
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 2;
+  const itemsPerPage = 6;
 
   const { data, error } = useSWR('/api/qna', fetcher);
   if (error) return <LoadFailed />;
@@ -30,7 +31,7 @@ export default function Qna() {
   return (
       <div>
         <header className="flex justify-center mb-5">
-          <h1 className="bracket-md text-center">常見問題<br />Q & A</h1>
+          <h1 className="bracket-md text-center">常見問題<br/>Q & A</h1>
         </header>
         <main>
           <Questions data={currentData} />
