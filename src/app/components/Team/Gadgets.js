4560d85e4098f8bb78b0884adbf433d5f0364e47
joan0802 <joan040802@gmail.com>
2023-08-09 13:30:53 +0800

    fix search bar & commend RWD

--- src/app/components/Team/Gadgets.js
+++ src/app/components/Team/Gadgets.js
@@ -3,13 +3,17 @@ import Search from "../../../../public/img/Search.svg"
 
 export function Search_bar(){
     return(
-        <div className="max-h-[5rem] max-w-[25rem] rounded-[4rem] nue-pressed p-6">
-            <div className="flex flex-row">
+        <div className="max-h-[5rem] max-w-[25rem] rounded-[4rem] search-shadow p-6">
+            <div className="flex flex-row space-x-4">
                 <Image
                     src={Search}
                     alt="Search icon"
                     className=""
                     priority
+                />
+                <input
+                    type="text"
+                    className="flex-grow outline-none bg-primary text-2xl"
                 />
             </div>
         </div>
@@ -18,7 +22,7 @@ export function Search_bar(){
 
 export function Post() {
     return(
-        <div className="max-h-[5rem] max-w-[9rem] rounded-[4rem] nue-concave py-4 w-full">
+        <div className="hidden md:block max-h-[5rem] max-w-[9rem] rounded-[4rem] nue-concave py-4 w-full">
             <h2 className="mx-auto my-auto text-accent">投稿</h2>
         </div>
     );
