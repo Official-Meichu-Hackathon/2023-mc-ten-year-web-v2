c1e0753af5a8751a6891d66a234e498b662b0f1f
joan0802 <joan040802@gmail.com>
2023-08-10 21:08:18 +0800

    add close button

--- src/app/components/Team/Team_Intro.js
+++ src/app/components/Team/Team_Intro.js
@@ -1,23 +1,21 @@
 "use client";
 import React from 'react';
+import Image from "next/image";
 import { Card_title } from "./Card_title";
 import { Project_Intro } from "./Project_Intro";
 import { Board } from "./Comments";
-import { useState, useEffect } from 'react';
 import close from "../../../../public/img/close.svg";
 
 export default function Team_Intro({ onClose, title, group, name, mail, github, tags, view, msg, discrip, comments }) {
-    const [popupTop, setPopupTop] = useState('50vh');
-    const toolbarHeight = window.outerHeight - window.innerHeight;
 
     return (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
-            <div className="fixed top-0 left-0 md:w-4/5 w-full h-fit bg-black bg-opacity-50 flex items-center justify-start" style={{ position: 'fixed', top: toolbarHeight }}>
+            <div className="fixed top-0 left-0 md:w-4/5 w-full h-fit bg-black bg-opacity-50 flex items-center justify-start overflow-auto" style={{ position: 'fixed', top: 0 }}>
                 {/* 彈出視窗內容 */}
                 <div className="w-full bg-primary overflow-y-auto">
-                    {/* <button className="float-right" onClick={onClose}>
-                        <Image src={close} alt="close" width={30} height={30} className="pt-4" />
-                    </button> */}
+                    <button className="float-right mt-16 mr-16" onClick={() => { console.log('Card clicked'); onClose();}}>
+                        <Image src={close} alt="close" width={30} height={30} className="" />
+                    </button>
                     <div className="">
                         <Card_title title={title} group={group} name={name} mail={mail} github={github} tags={tags} view={view} msg={msg} />
                         <Project_Intro discrip={discrip} />
--- src/app/layout.js
+++ src/app/layout.js
@@ -36,6 +36,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
 	return (
 		<html lang="en">
             <head>
