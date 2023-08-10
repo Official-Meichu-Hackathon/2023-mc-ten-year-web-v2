4f9e66d8328da0a4cfe885851424eb7963858206
joan0802 <joan040802@gmail.com>
2023-08-10 16:04:03 +0800

    fix pop up layout

--- src/app/components/Team/Team_Intro.js
+++ src/app/components/Team/Team_Intro.js
@@ -4,29 +4,21 @@ import { Card_title } from "./Card_title";
 import { Project_Intro } from "./Project_Intro";
 import { Board } from "./Comments";
 import { useState, useEffect } from 'react';
+import close from "../../../../public/img/close.svg";
 
 export default function Team_Intro({ onClose, title, group, name, mail, github, tags, view, msg, discrip, comments }) {
     const [popupTop, setPopupTop] = useState('50vh');
-    
-    useEffect(() => {
-        const toolbarHeight = window.scrollY;
-
-        // 設定彈出視窗的位置
-        const popupPosition = `${toolbarHeight}px`;
-        setPopupTop(popupPosition);
-    }, []);
+    const toolbarHeight = window.outerHeight - window.innerHeight;
 
     return (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
-            <div className="fixed top-0 left-0 md:w-4/5 w-full h-full bg-black bg-opacity-50 flex items-center justify-start" style={{ top: popupTop }}>
+            <div className="fixed top-0 left-0 md:w-4/5 w-full h-fit bg-black bg-opacity-50 flex items-center justify-start" style={{ position: 'fixed', top: toolbarHeight }}>
                 {/* 彈出視窗內容 */}
                 <div className="w-full bg-primary overflow-y-auto">
-                    {/* <button onClick={() => {
-                        onClose();
-                    }} className="absolute top-4 right-4 bg-red-500 text-white p-2 rounded-full">
-                        關閉
+                    {/* <button className="float-right" onClick={onClose}>
+                        <Image src={close} alt="close" width={30} height={30} className="pt-4" />
                     </button> */}
-                    <div className="pt-32">
+                    <div className="">
                         <Card_title title={title} group={group} name={name} mail={mail} github={github} tags={tags} view={view} msg={msg} />
                         <Project_Intro discrip={discrip} />
                     </div>
