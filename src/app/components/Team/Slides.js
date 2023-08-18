4e62b233cd94b942936c73e3de4f2162a2512c80
joan0802 <joan040802@gmail.com>
2023-08-18 17:53:06 +0800

    fix number bug

--- src/app/components/Team/Slides.js
+++ src/app/components/Team/Slides.js
@@ -64,13 +64,13 @@ export function ImageSlider({ closePopup, openPopup }) {
 
         <div className='absolute bottom-0 right-0 z-100 flex items-end space-y-2 mb-6 mr-6 space-x-4'>
 
-          <div className='bg-white border border-black rounded-full flex justify-center h-[2rem] w-[2rem] items-center cursor-pointer'>
+          <div className='bg-white border border-black rounded-full flex justify-center h-[2.2rem] w-[2.2rem] items-center cursor-pointer'>
             <button onClick={() => { openPopup() }} className='' >
               <Image src={fullscreen} alt="fullscreen" className="" />
             </button>
           </div>
 
-          <div className="bg-white border border-black rounded-[4rem] h-[2rem] aspect-1/3 flex items-center justify-center p-2 space-x-2">
+          <div className="bg-white border border-black rounded-[4rem] h-[2.2rem] aspect-1/3 flex items-center justify-center p-2 space-x-2">
             {/* Left Arrow */}
             <button className='cursor-pointer'
               onClick={prevSlide}>
@@ -78,7 +78,7 @@ export function ImageSlider({ closePopup, openPopup }) {
             </button>
 
             {/* Current Slide Index */}
-            <div className='text-lg font-bold'>
+            <div className='text-lg font-bold flex-shrink-0 w-[1rem] text-center'>
               {currentIndex + 1}
             </div>
 
