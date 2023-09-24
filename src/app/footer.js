e62ba07d2266b900ffa62c376302aae8babac1e1
r1407p <buszter0403@gmail.com>
2023-09-24 17:37:13 +0800

    feat: footer navbar

--- src/app/footer.js
+++ src/app/footer.js
@@ -8,7 +8,7 @@ import { faEnvelope } from "@fortawesome/free-regular-svg-icons"
 export default function Footer() {
 	return (
 		<footer className="bg-slate-50">
-			<div className="grid place-items-center gap-y-2 p-10">
+			<div className="grid place-items-center gap-y-2 p-4">
                 <div className="flex gap-x-6 [&_>_*]:transition-colors [&_>_*]:motion-reduce:transition-none">
                     <a id="fb-link" href="https://www.facebook.com/HackMeiChu" aria-label="Facebook" target="_blank" rel="noreferrer noopener"
                                     className="footer-link">
